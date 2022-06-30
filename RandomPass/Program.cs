907a3d4d5b924c3803f42d2b5307973ec2c783d2
RobinTao <taokuan@hotmail.com>
2022-06-30 19:57:14 +0800

    Initial

---
+++ RandomPass/Program.cs
@@ -0,0 +1,13 @@
+﻿using RandomPass;
+
+//int length = 32;
+//int nonAlphabet = 16;
+
+int length = 16;
+int nonAlphabet = 8;
+
+for (int i = 0; i < 10; ++i)
+{
+    string pass = Password.Generate(length, nonAlphabet);
+    Console.WriteLine(pass);
+}
